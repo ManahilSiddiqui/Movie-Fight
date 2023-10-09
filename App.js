1c37e3ccdefe481d787a73bdb5b127fd94bfab1e
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-09 20:31:55 +0400

    Featch Movie Data

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+ 
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '330151ac',
+            s: 'avengers'
+        }
+    });
+
+    console.log(response.data);
+ };
+
+ fetchData();

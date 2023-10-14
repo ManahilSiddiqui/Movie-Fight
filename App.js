62306b0068ea2efb7d81203570d8a05de65ca1de
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-14 22:39:00 +0400

    Removed move references

--- App.js
+++ App.js
@@ -1,36 +1,35 @@
- 
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '330151ac',
-            s: searchTerm
-        }
-    });
-
-    if (response.data.Error) {
-        return [];
-    }
-
-    return response.data.Search;
- };
 
 createAutoComplete({
     root: document.querySelector('.autocomplete'),
     renderOption(movie) {
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        return  `
-        <img src= "${imgSrc}" />
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+      return `
+        <img src="${imgSrc}" />
         ${movie.Title} (${movie.Year})
-        `
-
+      `;
     },
-    onOptionSelect(movie){
-        onMovieSelect(movie);
+    onOptionSelect(movie) {
+      onMovieSelect(movie);
     },
     inputValue(movie) {
-        return movie.Title;
+      return movie.Title;
+    },
+    async fetchData(searchTerm) {
+      const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+          apikey: '330151ac',
+          s: searchTerm
+        }
+      });
+  
+      if (response.data.Error) {
+        return [];
+      }
+  
+      return response.data.Search;
     }
-});
+  });
+
 
 const onMovieSelect = async movie => {
     const response = await axios.get('http://www.omdbapi.com/', {
@@ -38,7 +37,8 @@ const onMovieSelect = async movie => {
             apikey: '330151ac',
             i: movie.imdbID
         }
-    });
+        
+        });
 
     document.querySelector('#summary').innerHTML = movieTemplate(response.data);
 };
@@ -80,5 +80,4 @@ const movieTemplate = (movieDetail) => {
        <p class="subtitle">IMDB Votes</p>
     </article>
     `;
-
 };

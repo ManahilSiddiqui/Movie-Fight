8c6a30102bdf1c5b3326c469a35d84ac988563da
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-16 22:54:24 +0400

    showing two summaries

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-const autocompleteConfig = {
+const autoCompleteConfig = {
   renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
     return `
       <img src="${imgSrc}" />
       ${movie.Title} (${movie.Year})
     `;
-  },
-  onOptionSelect(movie) {
-    document.querySelector('.tutorial').classList.add('is-hidden');
-    onMovieSelect(movie);
   },
   inputValue(movie) {
     return movie.Title;
@@ -30,62 +26,69 @@ const autocompleteConfig = {
 };
 
 createAutoComplete({
-  ...autocompleteConfig,
-    root: document.querySelector('#left-autocomplete'),
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#left-summary'));
+  }
+});
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#right-summary'));
+  }
+});
+
+const onMovieSelect = async (movie, summaryElement) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '330151ac',
+      i: movie.imdbID
+    }
   });
-  createAutoComplete({
-    ...autocompleteConfig,
-      root: document.querySelector('#right-autocomplete'),
-    });
 
-
-const onMovieSelect = async movie => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '330151ac',
-            i: movie.imdbID
-        }
-        
-        });
-
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
 };
 
-const movieTemplate = (movieDetail) => {
-    return `
+const movieTemplate = movieDetail => {
+  return `
     <article class="media">
-    <figure class="media-left">
-    <p class="image">
-    <img src="${movieDetail.Poster}" />
-      </p>
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
       </figure>
       <div class="media-content">
-      <div class="content">
-        <h1>${movieDetail.Title}</h1>
-        <h4>${movieDetail.Genre}</h4>
-        <p>${movieDetail.Plot}</p>
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
         </div>
       </div>
     </article>
     <article class="notification is-primary">
-       <p class="title">${movieDetail.Awards}</p>
-       <p class="subtitle">Awards</p>
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
     </article>
     <article class="notification is-primary">
-       <p class="title">${movieDetail.BoxOffice}</p>
-       <p class="subtitle">Box Office</p>
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
     </article>
     <article class="notification is-primary">
-       <p class="title">${movieDetail.Metascore}</p>
-       <p class="subtitle">Metascore</p>
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
     </article>
     <article class="notification is-primary">
-       <p class="title">${movieDetail.imdbRating}</p>
-       <p class="subtitle">IMDB Rating</p>
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
     </article>
     <article class="notification is-primary">
-       <p class="title">${movieDetail.imdbVotes}</p>
-       <p class="subtitle">IMDB Votes</p>
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
     </article>
-    `;
+  `;
 };
+
